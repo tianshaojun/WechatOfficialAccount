a49accf830658561419a173e47e37c9192129285
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2022-10-09 14:59:43 +0800

    20221009mongodb环境搭建

--- Express/my-server/routes/index.js
+++ Express/my-server/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth', function (req, res) {
+  res.send('wecome to auth !!!!')
+})
+
+router.post('/reg', function (req, res) {
+  console.log(req.body) //接受前端通过post提交的数据
+})
 
 module.exports = router;
